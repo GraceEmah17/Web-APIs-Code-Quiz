dabf0a1b71c0a03d6676b41c9ec4ad19ff70ec21
Grace Emah <grace_esueti@yahoo.co.uk>
2024-01-02 13:43:34 +0000

    added array of quiz questions

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// Array of quiz questions with choices and correct answers
+const questions = [
+    {
+      question: "What is the capital of France?",
+      choices: ["Berlin", "Madrid", "Paris", "Rome"],
+      correctAnswer: "Paris",
+    },
+
+    
